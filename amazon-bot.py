e32e49c27bf4ba02b015a956c215e6979d9a55be
SoiledCold5 <48840847+SoiledCold5@users.noreply.github.com>
2021-05-23 18:23:29 -0400

    Update amazon-bot.py

--- amazon-bot.py
+++ amazon-bot.py
@@ -2,12 +2,7 @@ from selenium import webdriver
 from time import sleep
 
 CARD_TO_BUYS = [
-    "https://www.amazon.com/MSI-GeForce-RTX-3060-12G/dp/B08WPJ5P4R/ref=sr_1_2?dchild=1&keywords=3060+rtx&qid=1619637512&refinements=p_85%3A2470955011&rnid=2470954011&rps=1&sr=8-2",
-    "https://www.amazon.com/ASUS-Graphics-DisplayPort-Military-Grade-Certification/dp/B08WHJPBFX/ref=sr_1_1?dchild=1&keywords=asus+3060+rtx&qid=1619637575&sr=8-1",
-    "https://www.amazon.com/EVGA-GeForce-12G-P5-3657-KR-Dual-Fan-Backplate/dp/B08WM28PVH/ref=sr_1_2?dchild=1&keywords=evga+3060+rtx&qid=1619637588&sr=8-2",
-    "https://www.amazon.com/ZOTAC-GeForce-Graphics-IceStorm-ZT-A30600H-10M/dp/B08W8DGK3X/ref=sr_1_5?dchild=1&keywords=RTX+3060&qid=1619754618&sr=8-5",
-    "https://www.amazon.com/ZOTAC-GeForce-Graphics-IceStorm-ZT-A30600E-10M/dp/B08ZZW34T3/ref=sr_1_6?dchild=1&keywords=RTX+3060&qid=1619754618&sr=8-6",
-    "https://www.amazon.com/GIGABYTE-GeForce-Graphics-WINDFORCE-GV-N3060GAMING/dp/B08X4V8N5Q/ref=sr_1_7?dchild=1&keywords=RTX+3060&qid=1619754618&sr=8-7",
+    "https://www.amazon.com/GeForce-RTX-3060-12G-P5-3657-KR-Backplate/dp/B092L2KTHD/ref=sr_1_21?crid=QHGK99E7NQLU&dchild=1&keywords=rtx+3060&qid=1621808207&sprefix=rtx%2Caps%2C165&sr=8-21",
 ]
 SLEEP_IN_BETWEEN = 5
 
